4e3f4141ee8fcc1ac3e9a52bce82f88869e9c74c
Tsarina <tsarina.islam@gmail.com>
2019-07-30 10:45:35 -0400

    all orm functions except the update one

--- config/orm.js
+++ config/orm.js
@@ -4,33 +4,49 @@ var connection = require("../config/connection.js")
 // The below func assigns "?" for each variable necessar (as an array), then makes it a string so that it's in SQL format "(?, ?, ?)"
 function printQuestionMarks(num) {
     var arr = [];
-  
+
     for (var i = 0; i < num; i++) {
-      arr.push("?");
+        arr.push("?");
     }
-  
+
     return arr.toString();
-  }
+}
 
 //   The below function takes object key/value pairs and turns them into SQL syntax
 function objToSql(ob) {
     var arr = [];
-  
+
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+
+            arr.push(key + "=" + value);
         }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        
-        arr.push(key + "=" + value);
-      }
     }
-  
+
     // translate array of strings to a single comma-separated string
     return arr.toString();
-  }
+}
+
+var orm = {
+    selectAll: function (tableInput, callback) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+
+    insertOne: function(table, cols, vals, callback) {
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") "
+    }
+}
