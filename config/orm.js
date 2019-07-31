040fd58e6d5a09812a12d6068076ae8c79a53f2f
Tsarina <tsarina.islam@gmail.com>
2019-07-31 10:10:29 -0400

    finished orm.js functions, beginnings of burger.js

--- config/orm.js
+++ config/orm.js
@@ -47,6 +47,38 @@ var orm = {
     },
 
     insertOne: function(table, cols, vals, callback) {
-        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") "
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") "
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        })
+    },
+
+    updateOne: function(table, objColVals, condition, callback) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result;)
+        })
     }
 }
