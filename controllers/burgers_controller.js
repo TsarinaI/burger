a2433254e463f632b396b3a5a1419dc136fbd14f
Tsarina <tsarina.islam@gmail.com>
2019-08-02 11:10:32 -0400

    changes made w/tutor to fix gif and functionality of adding a burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,36 +3,28 @@ var router = express.Router();
 
 var burger = require("../models/burger.js")
 
-// Using "*", I learned in tutoring, is a better catch-all than "/"
-router.get("*", function (req, res) {
-    burger.selectAll(function(data) {
-        var burgersObj = {
-            burgers: data
-        };
-        console.log(burgersObj);
-        res.render("index", burgersObj);
-    });
-});
+
 
 // Post function to make new burger
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
+    console.log("testing", req.body)
     burger.insertOne([
         "burger_name", "devoured"
     ], [
-        req.body.burger_name, req.body.devoured
-    ], function(req, res) {
-        res.redirect("*")
-    })
+            req.body.burger_name, false/////here
+        ], function (results) { ////here
+            res.redirect("/")
+        })
 })
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result) {
+        devoured: true
+    }, condition, function (result) {
         if (result.changedRows === 0) {
             // if no rows were changed (id doesnt exist), send 404 message
             return res.status(404).end();
@@ -41,6 +33,15 @@ router.put("/api/burgers/:id", function(req, res) {
         }
     });
 });
-
+// Using /"
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var burgersObj = {
+            burgers: data
+        };
+        console.log(burgersObj);
+        res.render("index", burgersObj);
+    });
+});
 // Exporting routes for server.js
 module.exports = router;
